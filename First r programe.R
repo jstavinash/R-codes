0ba12e5e193c7b68caa7b728c43f28a91fae50cb
jstavinash <72619142+jstavinash@users.noreply.github.com>
2020-10-09 21:13:06 +0530

    Add files via upload

---
+++ First r programe.R
@@ -0,0 +1,36 @@
+1:10
+20:1
+
+a= 1+5
+print(a)
+1+5
+
+1+2*3
+
+sqrt(2)
+
+a<-2
+b<-3
+c<-1
+c+a*b
+my_vect<-c(2,3,7)
+my_vect1 <-c(a,b,c)
+a<-c(1,2,3,4)
+b<-c(5,6,7,8)
+a+b
+a-b
+a*4
+## ecludes values at position 1
+a[-1]
+
+a<-c(1,2,3,4)
+##replace the number at inde position with 10
+a[2]<-10
+
+## compares each element of the vector with 9
+a<9
+## edits the elements marked as true in inde vector
+a[a<9]=2
+
+print(a)
+a<10
